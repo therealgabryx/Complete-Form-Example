f3a44e90994c3abbd7cd3de1be6abc1fc3e484d1
Gabriele Concli <gabriele.concli@gmail.com>
2020-05-14 17:48:42 +0200

    code improvements

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
 function checkForm() {
-    if (document.registr.cognome.value == "") {
-        alert("Insert Surname");
+    if (document.register.name.value == "") {
+        alert("Insert a valid Name");
         return false;
     }
-    if (document.registr.nome.value == "") {
-        alert("Insert Name");
+
+    if (document.register.surname.value == "") {
+        alert("Insert a valid Surname");
         return false;
-    }
-    if (document.registr.dataN.value == "") {
-        alert("Inserirt date of birth");
+    }  
+
+    if (document.register.gender.value == "") {
+        alert("Insert a Gender");
         return false;
-    }
-    var v = parseInt(document.registr.matricola.value);
-    if (isNaN(v)) {
-        alert("Date not valid");
+    } 
+
+    if (document.register.birthdate.value == "") {
+        alert("Insert a Date of Birth");
         return false;
-    }
-    if (document.registr.regione.value == "nessuna") {
+    } 
+
+    if (document.register.region.value == "none") {
         alert("Select your Region");
         return false;
-    }
-    if ((document.registr.email.value == "") && (document.registr.tel.value == "")) {
-        alert("Insert e-mail or phone number");
+    } 
+
+    if ((document.register.email.value == "") && (document.register.phone.value == "")) {
+        alert("Insert Email or Phone Number");
         return false;
-    }
+    } 
+
+    if ((document.register.baseball.checked == false) && 
+        (document.register.tennis.checked == false) && 
+        (document.register.football.checked == false) && 
+        (document.register.basketball.checked == false) && 
+        (document.register.skiing.checked == false) && 
+        (document.register.coding.checked == false)) {
+        alert("Specify at least one preference");
+        return false;
+    } 
+
     alert("Data successfully inserted!");
-    return true;
-
-
-   
+    return true; 
 }
